e294c9a32ed208d6a856914ca981f1a6a8072653
Anthony Yip <anthonyjtyip@gmail.com>
2020-09-28 16:48:38 -0700

    commit build folder to ghpage

---
+++ precache-manifest.ac96b32690bb4259407ea71751e99f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbcb10d7e1167f523450a9786ebd3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a98e06b1732f67fb6a54",
+    "url": "/static/css/main.46dc329a.chunk.css"
+  },
+  {
+    "revision": "3a10c3f3e985e627315d",
+    "url": "/static/js/2.5886da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5886da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98e06b1732f67fb6a54",
+    "url": "/static/js/main.75efb93c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
