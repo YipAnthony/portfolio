6867def43ea4899a85511044221853c8e6a432b7
Anthony Yip <anthonyjtyip@gmail.com>
2020-10-08 07:56:07 -0700

    update resume

--- precache-manifest.bb702bb96664e0739c529182f15b00ea.js
+++ precache-manifest.bb702bb96664e0739c529182f15b00ea.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b662cbe969918d77fd05dd76f46e01e6",
+    "revision": "7906a82c949d838f18933fd41497d5ee",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "74230af2f5974ea24ec1",
+    "revision": "335dabaa184af2291e6d",
     "url": "/portfolio/static/css/main.b812b1cd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.5886da71.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "74230af2f5974ea24ec1",
-    "url": "/portfolio/static/js/main.dd48dae0.chunk.js"
+    "revision": "335dabaa184af2291e6d",
+    "url": "/portfolio/static/js/main.29026cef.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
