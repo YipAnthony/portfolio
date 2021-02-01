ccd481ea53bb195d7424753936e86f7a8f63a32f
AnthonyLegionLaptop <anthonyjtyip@gmail.com>
2021-02-01 13:18:57 -0800

    Deploy

---
+++ precache-manifest.12eca2ce9f96ed7692595cdbc5068117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbbc3075ac7bc57e7dba681a1158d8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2824acc92bdf7bf06b84",
+    "url": "/portfolio/static/css/2.18f4a77e.chunk.css"
+  },
+  {
+    "revision": "4b42902e258da4b60770",
+    "url": "/portfolio/static/css/main.7f3e6b12.chunk.css"
+  },
+  {
+    "revision": "2824acc92bdf7bf06b84",
+    "url": "/portfolio/static/js/2.11c8f1aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.11c8f1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b42902e258da4b60770",
+    "url": "/portfolio/static/js/main.fce89582.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
