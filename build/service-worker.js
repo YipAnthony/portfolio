9b3278404a9b74efbaffb77f9e189d987912ae6d
AnthonyLegionLaptop <anthonyjtyip@gmail.com>
2021-02-01 13:24:42 -0800

    reformat swiper pagination

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.12eca2ce9f96ed7692595cdbc5068117.js"
+  "/portfolio/precache-manifest.1fafe4daa8e2c01f30384844e12dad89.js"
 );
 
 self.addEventListener('message', (event) => {
